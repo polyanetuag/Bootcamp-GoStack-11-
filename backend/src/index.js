647e9eeef1f2c6f52633049e3dcab946f784e35d
Polyane Tuag <pollyportugues@hotmail.com>
2021-01-10 08:58:29 -0400

    métodos HTTP e suas rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,38 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello World'})
+// GET: Buscar informações do back-end
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+})
+
+// POST: Criar uma informação no back-end
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+})
+
+// PUT: Alterar uma informação no back-end
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+})
+
+// DELETE: Deletar uma informação no back-end
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3',
+  ]);
 })
 
 app.listen(3333, () => {
