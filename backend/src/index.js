a0949596a5728bc4ec06d01cdab3162f357afa27
Polyane Tuag <pollyportugues@hotmail.com>
2021-01-12 22:27:10 -0400

    atualizando as rotas para aplicação funcional

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid} = require('uuidv4');
 
 const app = express();
 
@@ -12,50 +13,66 @@ app.use(express.json());
   * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
 */
 
+const projects = [];
+
 // GET: Buscar informações do back-end
 app.get('/projects', (request, response) => {
-  const { title, owner} = request.query;      //query params
+  const { title} = request.query;      //query params - filtro
 
-console.log(title);
-console.log(owner);
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  return response.json(results);
 })
 
 
 // POST: Criar uma informação no back-end
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;                  //request body
-  console.log(title);
-  console.log(owner);
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const project = { id:uuid(), title, owner};
+
+  projects.push(project);
+
+  return response.json(project);      // exibido o projeto recém criado
 })
 
 // PUT: Alterar uma informação no back-end
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;            //route params
-  console.log(id)
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { title, owner } = request.body;  
+  
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.'})
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(project);      // retorna o projeto atualizado
 })
 
 // DELETE: Deletar uma informação no back-end
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 })
 
 app.listen(3333, () => {
