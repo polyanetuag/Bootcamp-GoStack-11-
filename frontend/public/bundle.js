9195ebb7f4aebe79c9d7b93175d68f6fa0915b15
Polyane Tuag <pollyportugues@hotmail.com>
2021-01-16 15:16:12 -0400

    configuração do babel

---
+++ frontend/public/bundle.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// tranformou o código que estava em index.js
+
+var soma = function soma(a, b) {
+  return a + b;
+};
+
+console.log(soma(1, 3));
---
+++ frontend/public/script.js
@@ -0,0 +1,7 @@
+/*
+  Babel: Converter(transpilar) código do React para um código que o browser entenda
+
+  Webpack: Pra cada tipo de arquivo(.js, .css, .png), eu vou converter o código de uma maneira diferente
+
+  Loaders: babel-loader, css-loader, image-loader  (conversores)
+*/
