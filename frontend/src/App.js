73eefd6a2e4cf1afea32bd66f55be94dce4ab9b6
Polyane Tuag <pollyportugues@hotmail.com>
2021-01-23 18:06:03 -0300

    Aprendendo sobre componentização

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {                // primeiro componente
+  return (                      // fragments
+    <>                         
+      <Header />
+      <Header />
+  </>
+  );
+}
+
+export default App;
