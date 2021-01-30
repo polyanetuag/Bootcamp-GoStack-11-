2b1558f9f0fcab79b393b6f2c6f5df0edf629b8e
Polyane Tuag <pollyportugues@hotmail.com>
2021-01-30 18:38:01 -0300

    Aprendendo a estilizar no react native

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+//As tags não possuem valor semântico
+//Não possuem estilização própria
+//Todos componentes possuem por padrão "display: flex"
+
+//View: div, footer, header, main, aside, section
+//text: p, span, strong, h1, h2, h3
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+    <StatusBar barStyle="light-content" />
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello GoStack</Text>
+    </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+
+})
