6f9e490a1da93dfe2debb5a34ade5bb05f7c87d0
Polyane Tuag <pollyportugues@hotmail.com>
2021-01-31 18:09:41 -0300

    listando projetos da Api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
 
-//As tags não possuem valor semântico
-//Não possuem estilização própria
-//Todos componentes possuem por padrão "display: flex"
-
-//View: div, footer, header, main, aside, section
-//text: p, span, strong, h1, h2, h3
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  },[]);
+
   return (
     <>
     <StatusBar barStyle="light-content" />
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello GoStack</Text>
-    </View>
+
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        data={projects}
+        keyExtractor={project => project.id}
+        renderItem={({ item: project}) => (
+          <Text style={styles.project}>{project.title}</Text>
+        ) }
+      />
+    </SafeAreaView>
     </>
   );
 }
@@ -23,13 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
 
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 32,
+    fontSize: 30,
     fontWeight: 'bold'
   },
 
